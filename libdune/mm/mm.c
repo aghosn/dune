8a111d1eac153ef7d724eed6a0a539b9fbd8da5e
aghosn <ghosn.adrien@gmail.com>
2016-11-03 12:05:36 +0100

    Working on VMAs
    Splitting and merging vmas is not trivial.
    Work in progress.

--- libdune/mm/mm.c
+++ libdune/mm/mm.c
@@ -16,8 +16,52 @@ uintptr_t mmap_base;
 uintptr_t stack_base;
 int dune_fd;
 
+/* Allocates and initializes a vm_area.
+ * WARNING: it does not add it to mm.
+ * WARNING: it does not change the pgroot mappings either.*/
+static vm_area_struct * mm_alloc_vma(mm_struct *mm,
+									vm_addrptr start,
+									vm_addrptr end,
+									unsigned long perm)
+{
+	vm_area_struct *vma = malloc(sizeof(vm_area_struct));
+	if (!vma)
+		return NULL;
+	vma->vm_start = start;
+	vma->vm_end = end;
+	vma->vm_flags = perm;
+
+	vma->vm_mm = mm;
+	Q_INIT_ELEM(vma, lk_areas);
+	return vma;
+}
+
+/* Removes the vmas between start (not included) and end (not included) for the mm->mmap.
+ * Frees the removed vmas.
+ * If start == NULL start at head.
+ * If end == NULL ends after last.
+ * WARNING: does not affect the page tables.*/
+static int mm_delete_region(mm_struct *mm,
+							vm_area_struct *start,
+							vm_area_struct *end)
+{
+	vm_area_struct *iter = (!start)? mm->mmap->head : start->lk_areas.next;
+	while (iter != NULL) {
+		vm_area_struct *tmp = iter;
+		iter = iter->lk_areas.next;
+		
+		Q_REMOVE(mm->mmap, tmp, lk_areas);
+		free(tmp);
+		if (iter == end)
+			break;
+	}
+
+	return 0;
+}
+
 //TODO change the permissions to use our own.
-int mm_init(bool map_full) {
+int mm_init(bool map_full)
+{
 	int ret;
 	void *start = NULL, *end = NULL, *pa = NULL;
 	unsigned long perm = 0;
@@ -52,7 +96,8 @@ int mm_create_mapping(	mm_struct *mm,
 						vm_addrptr va_start, 
 						vm_addrptr va_end, 
 						void* pa, 
-						unsigned long perm) {
+						unsigned long perm)
+{
 	vm_area_struct *current = NULL, *vma = NULL;
 	Q_FOREACH(current, mm->mmap, lk_areas) {
 		if (current->vm_start == va_start &&
@@ -62,7 +107,7 @@ int mm_create_mapping(	mm_struct *mm,
 			break;
 		}
 
-		if (mm_overlap(current, va_start, va_end, perm)) {
+		if (mm_overlap(current, va_start, va_end)) {
 			vma = mm_split_or_merge(current, va_start, va_end, perm);
 			break;
 		}
@@ -84,4 +129,136 @@ int mm_create_mapping(	mm_struct *mm,
 	
 	Q_INSERT_TAIL(mm->mmap, vma, lk_areas);
 	return 0;
+}
+
+
+int mm_overlap(vm_area_struct *vma, vm_addrptr start, vm_addrptr end)
+{
+	int dont_overlap = (vma->vm_end) < (start) || (vma->vm_start) >(end);
+	return !(dont_overlap);
+}
+
+int mm_split_or_merge(	mm_struct *mm,
+						vm_area_struct *vma,
+						vm_addrptr start,
+						vm_addrptr end,
+						unsigned long perm)
+{
+	assert(vma);
+	assert(start <= end);
+	vm_area_struct *iter = NULL, vm_area_struct *last = vma;
+	
+	/* Find the last conflicting block.*/
+	for (iter = vma->lk_areas.next; iter != NULL; iter = iter->lk_areas.next) {
+		if (!mm_overlap(iter, start, end)) {
+			last = iter->lk_areas.prev;
+			break;
+		}
+	}
+	
+	/*1. creates only one vma, it is a merge 
+	 *2. creates only two blocks.
+	 *3. creates three blocks.*/
+
+	/*1.*/
+	if (start <= vma->vm_start && end >= last->vm_end) {
+		vm_area_struct *nvma = mm_alloc_vma(vma->vm_mm, start, end, perm);
+		if (!nvma)
+			return NULL;
+		//TODO clean up the ones in-between.
+		//call the pgroot functions.
+		return 0;
+	}
+
+	/*2. TODO move the first up and handle better.*/
+	vm_addrptr f_start = (vma->vm_start < start)? vma->vm_start : start;
+	vm_addrptr f_end = (vma->vm_start > start)? vma->vm_start : start;
+	unsigned long f_perm = (vma->vm_start < start)? vma->vm_flags : perm;
+	
+	vm_area_struct *f_vma = mm_alloc_vma(vma->vm_mm, f_start, f_end, f_perm);
+	if (!f_vma)
+		goto err;
+
+	vm_addrptr s_start = f_end;
+	vm_addrptr s_end = (vma->vm_end <= end)? vma->vm_end : end;
+	unsigned long s_perm = (f_perm == perm)? vma->vm_flags : perm;
+
+	vm_area_struct *s_vma = mm_alloc_vma(vma->vm_mm, s_start, s_end, s_perm);
+	if (!s_vma)
+		goto err;
+
+	/*3.*/
+	vm_area_struct *t_vma = NULL;
+	vm_addrptr max_end = (vma->vm_end < end)? end : vma->vm_end;
+	if (s_end != max_end) {
+		vm_addrptr t_start = s_end;
+		vm_addrptr t_end = max_end;
+		unsigned long t_perm = f_perm;
+
+		t_vma = mm_alloc_vma(vma->mm_vm, t_start, t_end, t_perm);
+		if (!t_vma)
+			goto err;
+	}
+
+	//TODO remove everything between vma and last and free them.
+	//do vma->prev -> f_vma -> s_vma (-> t_vma) -> last.next
+	//apply everything on pageroot.
+	vma = vma->lk_areas.prev;
+	last = last->lk_areas.next;
+	mm_delete_region(mm, vma, last);
+	Q_INSERT_AFTER(mm->mmap, vma, f_vma, lk_areas);
+	Q_INSERT_AFTER(mm->mmap, f_vma, s_vma, lk_areas);
+	if (t_vma)
+		Q_INSERT_AFTER(mm->mmap, s_vma, t_vma, lk_areas);
+
+	//TODO apply pgroot for f_vma, s_vma, and (t_vma)? t_vma.
+	//TODO do this before.
+	if (mm_apply_to_pgroot(f_vma))
+		goto err;
+	if (s_vma && mm_apply_to_pgroot(s_vma))
+		goto err;
+	if (t_vma && mm_apply_to_pgroot(t_vma))
+		goto err;
+
+err:
+	if (f_vma)
+		free(f_vma);
+	if (s_vma)
+		free(s_vma);
+	if (t_vma)
+		free(t_vma);
+	return -ENOMEM;
+}
+
+int mm_apply_to_pgroot(vm_area_struct *vma)
+{
+	if (!vma || !(vma->vm_mm) || !(vma->vm_mm->pml4))
+		return -EINVAL;
+	
+	//TODO call proper dune page_walk of dune_map_phys or whatev'.
+	return 0;
+}
+
+// int mm_insert(mm_struct *mm, vm_area_struct *vma) {
+// 	if (!(mm->mmap->head)) {
+// 		Q_INSERT_TAIL(mm->mmap, vma);
+// 		return 0;
+// 	}
+	
+// 	l_vm_area *result = malloc(sizeof(l_vm_area));
+// 	if (!result)
+// 		return -ENOMEM;
+
+// 	vm_area_struct *iter = mm->mmap->head, vm_area_struct *new_vma = vma;
+// 	while(iter != NULL) {
+// 		if (iter->vm_end < new_vma->vm_start) {
+// 			vm_area_struct *tmp = iter;
+// 			iter = iter->lk_areas.next;
+// 			Q_INSERT_TAIL(result, tmp, lk_areas);
+// 		} else if (iter->vm_start > new_vma->vm_end) {
+// 			Q_INSERT_TAIL(result, new_vma, lk_areas);
+// 			new_vma = iter;
+// 		} else if ()
+// 	}
+
 }
--- libdune/mm/mm.h
+++ libdune/mm/mm.h
@@ -16,13 +16,13 @@ int mm_create_mapping(	mm_struct *mm,
 						void* pa, 
 						unsigned long perm);
 
-int mm_overlap(	vm_area_struct *vma, 
-				vm_addrptr s, 
-				vm_addrptr e, 
-				unsigned long perm);
+int mm_overlap(vm_area_struct *vma, vm_addrptr s, vm_addrptr e);
 
-vm_area_struct* mm_split_or_merge(	vm_area_struct *vma, 
-									vm_addrptr s, 
-									vm_addrptr e, 
+vm_area_struct* mm_split_or_merge(	mm_struct *mm,
+									vm_area_struct *vma, 
+									vm_addrptr start, 
+									vm_addrptr end, 
 									unsigned long perm);
+
+int mm_apply_to_pgroot(vm_area_struct *vma);
 #endif /*__LIBDUNE_MM_MM_H__*/
