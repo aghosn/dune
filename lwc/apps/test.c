57ffb5ee435893b5ee7641f18a41fa7a021b1256
aghosn <ghosn.adrien@gmail.com>
2016-11-26 19:45:31 +0100

    cleaning

--- lwc/apps/test.c
+++ lwc/apps/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inc/syscall.h>
 #include <core/lwc_types.h>
 
@@ -7,17 +8,15 @@ int main(int argc, char *argv[]) {
 	lwc_res_t result;
 	i = lwc_create(NULL, &result);
 	printf("Hello world");
-	fflush(stdout);
+	
 	/* We are the child.*/
-	if (i == 0) {
-		printf(" from the child.\n");
-		fflush(stdout);
-	} else if (i == 1) {
+	if (i == 1) {
 		printf(" from the parent.\n");
-		fflush(stdout);
 		lwc_switch(result.n_lwc, NULL, &result);
+	} else if (i == 0) {
+		printf(" from the child.\n");
 	} else {
-		printf("Error\n");
+		printf("Error.\n");
 		return -1;
 	}
 
